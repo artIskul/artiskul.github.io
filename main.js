7fc3ddb9ba43be440f431f1c89088d01fb9d3acd
artIskul <64956236+artIskul@users.noreply.github.com>
2025-10-19 17:53:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,109 +5,91 @@ Gmail:yelsantos0@gmail.com
 */
 
 
-<script>
-  // ---------- NAVIGATION MENU TOGGLE ----------
-  let selectElement = (e) => document.querySelector(e);
+let selectElement = (e) => document.querySelector(e);
 
-  selectElement('.open').addEventListener('click', () => {    
-      selectElement('.nav-list').classList.add('active');
+selectElement('.open').addEventListener('click', () => {    
+    selectElement('.nav-list').classList.add('active');
+});
+
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
+
+selectElement('#close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
+
+
+// Filter Elements
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
   });
+}
 
-  selectElement('.close').addEventListener('click', () => {
-      selectElement('.nav-list').classList.remove('active');
+
+
+//code for bar chart
+setTimeout(function start (){
+  
+  $('.bar').each(function(i){  
+    var $bar = $(this);
+    $(this).append('<span class="count"></span>')
+    setTimeout(function(){
+      $bar.css('width', $bar.attr('data-percent'));      
+    }, i*100);
   });
-
-  selectElement('#close').addEventListener('click', () => {
-      selectElement('.nav-list').classList.remove('active');
-  });
-
-
-  // ---------- FILTER FUNCTION ----------
-  filterSelection("all");
-  function filterSelection(c) {
-    var x, i;
-    x = document.getElementsByClassName("filterDiv");
-    
-    // Default case: only show elements with 'default-show'
-    if (c == "all") {
-      for (i = 0; i < x.length; i++) {
-        if (x[i].classList.contains("default-show")) {
-          w3AddClass(x[i], "show");
-        } else {
-          w3RemoveClass(x[i], "show");
+ 
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).parent('.bar').attr('data-percent')
+    }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now) +'%');
         }
-      }
-      return;
-    }
-
-    // Category filter
-    for (i = 0; i < x.length; i++) {
-      w3RemoveClass(x[i], "show");
-      if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-    }
-  }
-
-  function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) {
-        element.className += " " + arr2[i];
-      }
-    }
-  }
-
-  function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-        arr1.splice(arr1.indexOf(arr2[i]), 1);
-      }
-    }
-    element.className = arr1.join(" ");
-  }
-
-
-  // ---------- ACTIVE CLASS TO BUTTON ----------
-  var btnContainer = document.getElementById("myBtnContainer");
-  var btns = btnContainer.getElementsByClassName("btn");
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function(){
-      var current = document.getElementsByClassName("active");
-      if (current.length > 0) {
-        current[0].className = current[0].className.replace(" active", "");
-      }
-      this.className += " active";
     });
-  }
-
-
-  // ---------- BAR CHART ANIMATION ----------
-  setTimeout(function start (){
-    $('.bar').each(function(i){  
-      var $bar = $(this);
-      $(this).append('<span class="count"></span>')
-      setTimeout(function(){
-        $bar.css('width', $bar.attr('data-percent'));      
-      }, i*100);
-    });
-
-    $('.count').each(function () {
-      $(this).prop('Counter',0).animate({
-          Counter: $(this).parent('.bar').attr('data-percent')
-      }, {
-          duration: 2000,
-          easing: 'swing',
-          step: function (now) {
-              $(this).text(Math.ceil(now) +'%');
-          }
-      });
-    });
-  }, 500);
-</script>
+});
+ 
+}, 500)
 
 
 
